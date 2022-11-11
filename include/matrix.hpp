f3a38630d38df7212b9727ee11bbdc48727c4dca
Pablo <parequena8@gmail.com>
2022-11-11 12:12:18 +0100

    Operators + Ctors

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <stdexcept>
 #include <initializer_list>
+#include <iostream>
 
 namespace tinyTools
 {
@@ -18,6 +19,7 @@ struct matrix
     using value = T;
     using reference = value&;
     using const_reference = value const&;
+    using container_type = std::array<T, ROWS * COLS>;
 
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
     // Ctors.
@@ -28,31 +30,51 @@ struct matrix
         for(std::size_t i {}; auto const l : list) { data_[i] = l; ++i; }
     }
 
+    inline constexpr matrix(container_type const& array) : data_{ array } { }
+    inline constexpr matrix(container_type&& array) { std::swap(data_, array); }
+    inline constexpr matrix(matrix const& rhm) : matrix( rhm.data_ ) { }
+    inline constexpr matrix(matrix&& rhm) { std::swap(data_, rhm.data_); } 
+    inline constexpr auto operator=(matrix const& rhm) -> matrix& { return *this = matrix(rhm); }
+    inline constexpr auto operator=(matrix&& rhm) noexcept -> matrix& { std::swap(data_, rhm.data_); return *this; }
+
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
     // Getters.
     inline auto getData() const noexcept -> std::array<T, ROWS * COLS> { return data_; }
 
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
     // Operators.
+    template <numerical L_T, std::size_t L_ROWS, std::size_t L_COLS>
+    friend std::ostream& operator<<(std::ostream& os, matrix<L_T, L_ROWS, L_COLS> const& rhm);
     inline constexpr auto operator()(std::size_t r, std::size_t c) const -> const_reference { return op_parenthesis(*this, r, c); }
     inline constexpr auto operator()(std::size_t r, std::size_t c)       -> reference       { return op_parenthesis(*this, r, c); }
 
+    inline constexpr auto operator[](std::size_t idx) const -> const_reference { return op_sqBracket(*this, idx); }
+    inline constexpr auto operator[](std::size_t idx)       -> reference       { return op_sqBracket(*this, idx); }
+
+    inline constexpr auto operator==(matrix const& rhm) const noexcept -> bool { for(std::size_t i{}; i < totalSize_; ++i) { if(data_[i] != rhm.data_[i]) { return false; } } return true; }
+    inline constexpr auto operator!=(matrix const& rhm) const noexcept -> bool { return !(operator==(rhm)); }
+    inline constexpr auto  operator<(matrix const& rhm) const noexcept -> matrix<bool, ROWS, COLS> { matrix<bool, ROWS, COLS> ret{}; for(std::size_t i{}; i < totalSize_; ++i) { ret[i] = data_[i]  < rhm.data_[i]; } return ret; }
+    inline constexpr auto operator<=(matrix const& rhm) const noexcept -> matrix<bool, ROWS, COLS> { matrix<bool, ROWS, COLS> ret{}; for(std::size_t i{}; i < totalSize_; ++i) { ret[i] = data_[i] <= rhm.data_[i]; } return ret; }
+    inline constexpr auto  operator>(matrix const& rhm) const noexcept -> matrix<bool, ROWS, COLS> { matrix<bool, ROWS, COLS> ret{}; for(std::size_t i{}; i < totalSize_; ++i) { ret[i] = data_[i]  > rhm.data_[i]; } return ret; }
+    inline constexpr auto operator>=(matrix const& rhm) const noexcept -> matrix<bool, ROWS, COLS> { matrix<bool, ROWS, COLS> ret{}; for(std::size_t i{}; i < totalSize_; ++i) { ret[i] = data_[i] >= rhm.data_[i]; } return ret; }
+
+    inline constexpr auto operator+=(matrix const& rhm) noexcept -> matrix& { for(std::size_t i{}; i < totalSize_; ++i) { data_[i] += rhm.data_[i]; } return *this; }
+    friend constexpr auto operator+(matrix lhm, matrix const& rhm) noexcept -> matrix { lhm += rhm; return lhm; }
+    inline constexpr auto operator+=(T const& scalar) noexcept -> matrix&   { for(std::size_t i{}; i < totalSize_; ++i) { data_[i] += scalar; } return *this; }
+    friend constexpr auto operator+(matrix lhm, T const& scalar) noexcept -> matrix { lhm += scalar; return lhm; }
+
+    inline constexpr auto operator-=(matrix const& rhm) noexcept -> matrix& { for(std::size_t i{}; i < totalSize_; ++i) { data_[i] -= rhm.data_[i]; } return *this; }
+    friend constexpr auto operator-(matrix lhm, matrix const& rhm) noexcept -> matrix { lhm -= rhm; return lhm; }
+    inline constexpr auto operator-=(T const& scalar) noexcept -> matrix&   { for(std::size_t i{}; i < totalSize_; ++i) { data_[i] -= scalar; } return *this; }
+    friend constexpr auto operator-(matrix lhm, T const& scalar) noexcept -> matrix { lhm -= scalar; return lhm; }
+
+
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
     // Methods.
-    void print() const noexcept
-    {
-        std::size_t i { 0 };
-        for(auto const d : data_)
-        {
-            std::cout << d;
-            if( ++i == COLS ) { std::cout << '\n'; i = 0; }
-            else              { std::cout << ' '; }
-        }
-    }
-    
+
 private:
     static constexpr std::size_t totalSize_ { ROWS * COLS };
-    std::array<T, totalSize_> data_{};
+    container_type data_{};
 
     // -----------------------------------------------------------------------------------------------------------------------------------------------------
     // Deducing This (C++20 Style). TODO: C++23
@@ -63,8 +85,32 @@ private:
         if(c > COLS) { throw std::out_of_range( std::string{ "Cols out of range, max cols= " + std::to_string(COLS) + '\n' } ); }
         return instance.data_[r * COLS + c];
     }
+
+    template<typename This>
+    static inline constexpr auto op_sqBracket(This &instance, std::size_t idx) -> auto&
+    {
+        if(idx > totalSize_) { throw std::out_of_range( std::string{ "Index out of range, max= " + std::to_string(totalSize_) + '\n' } ); }
+        return instance.data_[idx];
+    }
 };
 
+template <numerical L_T, std::size_t L_ROWS, std::size_t L_COLS>
+std::ostream& operator<<(std::ostream& os, matrix<L_T, L_ROWS, L_COLS> const& rhm)
+{
+    std::size_t i { 0 };
+    for(auto const d : rhm.data_)
+    {
+        os << d;
+        if( ++i == L_COLS ) { os << '\n'; i = 0; }
+        else              { os << ' '; }
+    }
+    return os;
+}
+
+/*
+0 1 2
+3 4 5
+*/
 
 } // namespace tinyTools
 
--- test/test.cpp
+++ test/test.cpp
@@ -14,19 +14,9 @@ protected:
         mat_A(1, 0) = 3;
         mat_A(1, 1) = 4;
         mat_A(1, 2) = 5;
-
-        mat_A.print();
     }
 
     tinyTools::matrix<int, 2, 3> mat_A{};
-
-    template <typename T>
-    void printArray(T const& array)
-    {
-        std::cout << "[ ";
-        for( auto const a : array) { std::cout << a << ' '; }
-        std::cout << "]\n";
-    }
 
     template <typename T>
     void compareArrays(T const& lha, T const& rha)
@@ -42,16 +32,24 @@ protected:
     }
 };
 
+
 TEST_F(TestMatrix, DumpyTest)
 {
     tinyTools::matrix<int, 2, 3> mat{};
     mat(0, 0) = 3;
-    auto const& array = mat.getData();
-    printArray(array);
+    [[maybe_unused]] auto const& array = mat.getData();
 }
 
 TEST_F(TestMatrix, Test_InitList_Ctor)
 {
     tinyTools::matrix<int, 2, 3> mat_B { 0, 1, 2, 3, 4, 5 };
-    compareArrays(mat_A.getData(), mat_B.getData());
+    EXPECT_TRUE(mat_A == mat_B);
+}
+
+TEST_F(TestMatrix, Test_Array_Ctor)
+{
+    constexpr std::size_t cols { 2 };
+    constexpr std::size_t rows { 3 };
+    std::array<int, rows * cols> data{ 0, 1, 2, 3, 4, 5 };
+    tinyTools::matrix<int, rows, cols> mat{ std::move(data) };
 }
